b2331ac657f1dac57da25789cd7fcd38eb44204a
sherwin-dc <swd18@ic.ac.uk>
2020-10-30 21:06:55 +0800

    svmake

---
+++ svmake.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ $# > 0 ]; then
+    for i in "$@"
+    do
+        if [ "$i" == "--help" ] || [ "$i" == "-h" ]; then
+            echo -e "\nCommands: svmake -s topfile.v [-w waveform.vcf]\n"
+            echo -e "\nAll .v and .sv files in the current folder and any subfolders will automatically be added as source files\n"
+            exit 0
+        fi
+        if [ "$i" == "-s" ]; then
+            shift
+            if [ $# > 0 ]; then
+                topmod=$1
+            fi
+        fi
+        if [ "$i" == "-w" ]; then
+            shift
+            if [ $# > 0 ]; then
+                waveform=$1
+            fi
+        fi
+    done    
+fi
+
+
+
+sourcefiles=$(find . -type f -iname "*.sv" -o -type f -iname "*.v")
+
+srcfiles=$(echo $srcfiles | tr '\n' ' ')
+
+if [ -n "$topmod" ]; then
+iverilog -Wall -g2012 -s $topmod -o $srcfiles
+./$topmod
+else
+echo -e "Please specify top level file with -s flag. Use with --help for more details."
+exit 1
+fi
+
+if [ -n "$waveforms" ]; then
+    gtkwave $waveforms
+fi
